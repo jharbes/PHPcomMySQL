fc2e3f9efade79cf8ab614814f3f0dcbcd75c853
Jorge Harbes <jharbes@hotmail.com>
2022-09-12 15:22:56 -0300

    atualizacao projeto

--- rodape.php
+++ rodape.php
@@ -2,8 +2,13 @@
 
 echo "<footer>";
 
-echo "<p>Acessado por ".$_SERVER['REMOTE_ADDR']." em ".date('d/m/Y')."</p>";
-echo "<p>Desenvolvido por Jorge Harbes em 2022</p>";
+echo "<p>Acessado por <strong>".$_SERVER['REMOTE_ADDR']."</strong> em <strong>".date('d/m/Y')."</strong></p>";
+echo "<p>Desenvolvido por <strong><a href='mailto:jharbes@hotmail.com'>Jorge Harbes</a></strong> em 2022</p>";
+echo "<p>
+
+<a href='https://www.linkedin.com/in/jorge-harbes-0664161a7/' target='_blank'><img class='redes-sociais' src='imagens/256x256bb.png' alt='LinkedIn'></a>
+
+<a href='https://github.com/jharbes/' target='_blank'><img class='redes-sociais' src='imagens/logo-github.jpg' alt='GitHub'></a></p>";
 
 echo "</footer>";
 
