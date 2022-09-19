d08c90e83f74a2966f6954bab2a775d40ae1b916
Jorge Harbes <jharbes@hotmail.com>
2022-09-19 15:49:35 -0300

    Update user-login-form.php

--- user-login-form.php
+++ user-login-form.php
@@ -1,8 +1,8 @@
 <h2>Login</h2>
 <form action="user-login.php" method="post">
     <table>
-        <tr><td>Usuário: </td><td><input type="text" name="usuario" id="usuario" size="10" maxlength="10" required></td></tr>
-        <tr><td>Senha: </td><td><input type="password" name="senha" id="senha" size="10" maxlength="8" required></td></tr>
+        <tr><td><label for="usuario">Usuário: </label></td><td><input type="text" name="usuario" id="usuario" size="10" maxlength="10" required></td></tr>
+        <tr><td><label for="senha">Senha: </label></td><td><input type="password" name="senha" id="senha" size="10" maxlength="8" required></td></tr>
         <tr><td><input type="submit" value="Entrar" id="botao-entrar"></td></tr>
     </table>
 </form>
