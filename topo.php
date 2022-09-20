c169a31b7b21b736340fcd038fa1e4476c7e8b16
Jorge Harbes <jharbes@hotmail.com>
2022-09-20 09:19:13 -0300

    Update topo.php

--- topo.php
+++ topo.php
@@ -6,7 +6,7 @@ if (empty($_SESSION['user']))
     echo "<a href='user-login.php' style='margin:10px;'>Entrar</a>";
 else{
     echo "Olá, <strong>".$_SESSION['nome']."</strong>! |  (usuário do tipo ".$_SESSION['tipo'].") ";
-    echo "Meus Dados | "
+    echo "Meus Dados | ";
     echo "<a href='user-logout.php'>Sair</a>";
 }
 
